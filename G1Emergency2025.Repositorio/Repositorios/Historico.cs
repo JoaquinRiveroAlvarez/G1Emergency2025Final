00301789e412b13927317994030889fc21c38a9e
EMILSE CLAUDIA ferrer <emilseferrer1@hotmail.com>
2025-11-11 10:47:49 -0300

    REPO HISTORICO

---
+++ G1Emergency2025.Repositorio/Repositorios/Historico.cs
@@ -0,0 +1,25 @@
+﻿using G1Emergency2025.Shared.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1Emergency2025.BD.Datos.Entity
+{
+    public class Historico : EntityBase
+    {
+
+        [Required(ErrorMessage = "La fecha de entrada es obligatoria.")]
+        public required DateTime FechaEntrada { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "La fecha de salida de es obligatoria.")]
+        public required DateTime FechaSalida { get; set; }
+
+        public int MovilId { get; set; }
+        public Movil? Movil { get; set; }
+        public int TripulanteId { get; set; }
+        public Tripulante? Tripulantes { get; set; }
+    }
+}
